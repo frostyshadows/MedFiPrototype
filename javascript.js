c226e9c8391f79d8ca1e9e8e750f4516357720ee
Sherry Yuan <frostyyshadows@gmail.com>
2017-11-19 09:58:01 -0800

    fixed hover

--- javascript.js
+++ javascript.js
@@ -73,59 +73,41 @@ $(function () {
 	var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+$("#myBtn").onclick = function() {
+    modal[0].style.display = "block";
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal[0].style.display = "none";
     }
-}
+};
 
 var modal1 = document.getElementById('myModal1');
-// Get the button that opens the modal
-var btn1 = document.getElementById("myBtn1");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn1.onclick = function() {
-    modal1.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal1.style.display = "none";
-}
+$("#myBtn1").onclick = function() {
+    modal1[0].style.display = "block";
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal1) {
-        modal1.style.display = "none";
+        modal1[0].style.display = "none";
     }
-}
+};
 
     $("#profile_button")
         .mouseenter(function () {
-        console.log("entered profile button");
         $("#profile_hover")[0].style.opacity = "1";
     })
         .mouseleave(function () {
-        console.log("left profile button");
         $("#profile_hover")[0].style.opacity = "0";
-    });
+    })
+        .click(function () {
+            window.open("ReviewerDetails.html", "_self");
+        });
 });
