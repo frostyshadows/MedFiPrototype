9dcbc5958865bc0535afcca7e920ec57fa665912
Minjia Zhan <minjiazhan@gmail.com>
2017-11-18 21:01:03 -0800

    Finished Vote Function

--- javascript.js
+++ javascript.js
@@ -48,6 +48,18 @@ $(function () {
     $("#cancel_button").click(function() {
         $("#super_reviewer_info")[0].style.display = "none";
     });
+	
+	$("#cancel_button1").click(function() {
+    $("#myModal1")[0].style.display = "none";
+    });
+	$("#cancel_button2").click(function() {
+    $("#myModal")[0].style.display = "none";
+    });
+	
+	$("#submit_button").click(function() {
+    $("#myModal")[0].style.display = "none";
+    });
+
 
     /* home page button */
     $(".home_page").click(function() {
@@ -57,6 +69,55 @@ $(function () {
             window.open("../index.html", "_self");
         }
     });
+	
+	var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal1 = document.getElementById('myModal1');
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
 
     $("#profile_button")
         .mouseenter(function () {
